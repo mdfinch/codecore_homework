7ec013c0c55fcad0c9b699475a0dabab6f3f543f
Michael Finch <michael.donovan.finch@gmail.com>
2020-03-21 19:23:59 -0700

    add loop to determine spaces

--- box_it_script/boxIt.js
+++ box_it_script/boxIt.js
@@ -5,10 +5,17 @@ const drawMiddleBorder = num => newLine + '\u2523' + drawLine(num) + '\u252b' + 
 const drawBottomBorder = num =>  newLine + '\u2517' + drawLine(num) + '\u251b' 
 const drawBarsAround = string => newLine + '\u2503' + string + '\u2503' + newLine
 
+
 function boxIt(array){
-    let arr;
-    let space = 0;
-     array[0] === undefined ? drawTopBorder() + drawBottomBorder() : arr = array.filter(elm => (elm !== undefined))
+   let arr;
+   let space = 0;
+    array[0] === undefined ? drawTopBorder() + drawBottomBorder() : arr = array.filter(elm => (elm !== undefined))
+
+    for(let x = 0; x < arr.length; x++){
+        if (space < arr[x].length) {
+            space = arr[x].length
+        }
+    }
 }
 
 console.log(boxIt([process.argv[2],process.argv[3],process.argv[4]]))
