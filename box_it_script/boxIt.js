cc7bfa4e17ccf0d13da2713a0ed5958e4e35feea
Michael Finch <michael.donovan.finch@gmail.com>
2020-03-21 19:40:56 -0700

    add semicolons

--- box_it_script/boxIt.js
+++ box_it_script/boxIt.js
@@ -1,15 +1,15 @@
-const newLine = '\n'
-const drawLine = (num) => '\u2501'.repeat(num)
-const drawTopBorder = num => '\u250f' + drawLine(num) + '\u2513' + newLine
-const drawMiddleBorder = num => newLine + '\u2523' + drawLine(num) + '\u252b' + newLine
-const drawBottomBorder = num =>  newLine + '\u2517' + drawLine(num) + '\u251b' 
-const drawBarsAround = string => newLine + '\u2503' + string + '\u2503' + newLine
+const newLine = "\n";
+const drawLine = (num) => "\u2501".repeat(num);
+const drawTopBorder = num => "\u250f" + drawLine(num) + "\u2513" + newLine;
+const drawMiddleBorder = num => newLine + "\u2523" + drawLine(num) + "\u252b" + newLine;
+const drawBottomBorder = num =>  newLine + "\u2517" + drawLine(num) + "\u251b" ;
+const drawBarsAround = string => newLine + "\u2503" + string + "\u2503" + newLine;
 
 
 function boxIt(array){
    let arr;
    let space = 0;
-    array[0] === undefined ? drawTopBorder() + drawBottomBorder() : arr = array.filter(elm => (elm !== undefined))
+    array[0] === undefined ? drawTopBorder() + drawBottomBorder() : arr = array.filter(elm => (elm !== undefined));
 
     for(let x = 0; x < arr.length; x++){
         if (space < arr[x].length) {
@@ -26,4 +26,4 @@ function boxIt(array){
     return box
 }
 
-console.log(boxIt([process.argv[2],process.argv[3],process.argv[4]]))
+console.log(boxIt([process.argv[2],process.argv[3],process.argv[4]]));
