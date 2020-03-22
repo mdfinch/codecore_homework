3f6736bda1ce1a569c819d600eea7eba7b13f62a
Michael Finch <michael.donovan.finch@gmail.com>
2020-03-21 19:20:14 -0700

    add filter for undefined input

--- box_it_script/boxIt.js
+++ box_it_script/boxIt.js
@@ -4,3 +4,11 @@ const drawTopBorder = num => '\u250f' + drawLine(num) + '\u2513' + newLine
 const drawMiddleBorder = num => newLine + '\u2523' + drawLine(num) + '\u252b' + newLine
 const drawBottomBorder = num =>  newLine + '\u2517' + drawLine(num) + '\u251b' 
 const drawBarsAround = string => newLine + '\u2503' + string + '\u2503' + newLine
+
+function boxIt(array){
+    let arr;
+    let space = 0;
+     array[0] === undefined ? drawTopBorder() + drawBottomBorder() : arr = array.filter(elm => (elm !== undefined))
+}
+
+console.log(boxIt([process.argv[2],process.argv[3],process.argv[4]]))
