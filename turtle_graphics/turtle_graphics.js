2a7f434717d995d4f71983a8980fa761bd02645b
Michael Finch <michael.donovan.finch@gmail.com>
2020-03-27 19:50:41 -0700

    add class Turtle

---
+++ turtle_graphics/turtle_graphics.js
@@ -0,0 +1,10 @@
+class Turtle {
+    constructor(x,y) {
+        this.x = x; // sets this turtle's starting x
+        this.y = y; // sets this turtle's starting y
+        this.direction = "East"; // Sets initial direction
+        this.array = []; // stores coordinates
+
+    }
+    
+}
